ccee08b6d06798771f9f587f51c7b2ae0d93bad4
bandilbulbul <bulbul.bandil@srsconsultinginc.com>
2021-06-15 13:44:59 +0530

    updated the code

--- helloo/helloo_test.go
+++ helloo/helloo_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestGreetsGitHub(t *testing.T) {
 	result := Greet()
-	if result != "ello GitHub Actions. Dev.to is awesome" {
+	if result != "Hello GitHub Actions. Dev.to is awesome" {
 		t.Errorf("Greet() = %s; want ello GitHub Actions. Dev.to is awesome", result)
 	}
 }
